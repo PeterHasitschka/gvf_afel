3a15396851d10fd4831b810fa34c2845cbe46807
Peter Hasitschka <phasitschka@know-center.at>
2017-01-24 17:44:11 +0100

    Further work on community graph

--- afel/graph/edges/learnerlearning.ts
+++ afel/graph/edges/learnerlearning.ts
@@ -3,7 +3,7 @@ import {Plane} from "../../../gvfcore/components/plane/plane";
 import {NodeLearner} from "../nodes/learner";
 export class EdgeLearnersLearning extends EdgeColored {
 
-    protected static color = 0xaaaaff;
+    protected static color = 0x5555ff;
 
     constructor(learnerNode1:NodeLearner, learnerNode2:NodeLearner, plane:Plane) {
         super(learnerNode1, learnerNode2, plane, EdgeLearnersLearning.color);
