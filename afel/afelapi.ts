988a6ed60b71b40ec9a306fe80fc2d65ad9d1e30
Peter Hasitschka <phasitschka@know-center.at>
2017-01-27 11:40:47 +0100

    R-Graph bg

--- afel/afelapi.ts
+++ afel/afelapi.ts
@@ -43,7 +43,7 @@ export class AfelApi implements GvfPluginInterface {
 
         GraphVisConfig["afel"] = {
             samelearning_tolerance: 0.95,
-            resourcegraph_background : 0x4444aa
+            resourcegraph_background : 0x8888aa
         }
 
 
