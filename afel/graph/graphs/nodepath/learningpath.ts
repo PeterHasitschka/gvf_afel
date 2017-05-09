6f20942eab3e0627b3a5308bb1f68f189aa6fd6e
Peter Hasitschka <phasitschka@know-center.at>
2017-05-09 13:12:56 +0200

    Color transition in learning path

--- afel/graph/graphs/nodepath/learningpath.ts
+++ afel/graph/graphs/nodepath/learningpath.ts
@@ -3,13 +3,15 @@ import {NodeAbstract} from "../../../../gvfcore/components/graphvis/graphs/nodes
 import {Plane} from "../../../../gvfcore/components/plane/plane";
 export class LearningPath extends NodepathAbstract {
 
-    protected static lineColor = 0x00AAAA;
+    protected static lineColor1 = 0x00FF00;
+    protected static lineColor2 = 0x0000FF;
     protected static lineWidth;
     protected static opacity = 0.8;
 
     constructor(nodesToConnect:NodeAbstract[], plane:Plane) {
         super(nodesToConnect, plane, {
-            lineColor: LearningPath.lineColor,
+            lineColor1: LearningPath.lineColor1,
+            lineColor2: LearningPath.lineColor2,
             lineWidth: LearningPath.lineWidth,
             opacity: LearningPath.opacity
         });
